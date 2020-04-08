c9d7235923dd79b4bb163a0b979994301b9da325
Jorge Ramirez <joramirez96@gmail.com>
2020-04-08 18:28:43 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,16 @@
 class User
-  
+
+  def initialize (first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end 
 
 end
